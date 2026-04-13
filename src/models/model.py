6f3b4643e3d1754166c7dabaa84236d030192bc1
Vlad <vovanikolaev140707@gmail.com>
2026-04-13 16:29:23 +0300

    added tables

--- src/models/model.py
+++ src/models/model.py
@@ -1,7 +1,32 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, func
 
 class Base(DeclarativeBase):
     pass
 
 
+class Users(Base):
+    __tablename__="users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    telegram_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
 
+    backups: Mapped[list["Backups"]] = relationship(back_populates="user")
+
+
+class Backups(Base):
+    __tablename__="backups"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    path: Mapped[int] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(default=func.now())
+
+    user: Mapped["Users"] = relationship(back_populates="backups")
+
+
+#class UserSettings(Base):
+#    __tablename__="settings"
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-uv run uvicron src.main:app --port 8000 --host "0.0.0.0"
+uv run uvicron src.main:app --port 8000 --host 0.0.0.0
 
 
