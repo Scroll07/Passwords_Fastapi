e0acd069de0d0ab975d195145ac0eb3d373966c2
Vlad <vovanikolaev140707@gmail.com>
2026-06-01 14:44:32 +0300

    moved routers to folder "routers"

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ from fastapi.staticfiles import StaticFiles
 
 from src.core.database import async_engine
 
-
-from src.api.passwords import passwords as pass_router
-from src.api.users import users as users_router
-from src.api.static_pages import pages
+from src.routers.api.passwords import api_passwords
+from src.routers.api.users import api_users
+from src.routers.web.passwords import web_passwords
+from src.routers.web.users import web_users
+from src.routers.web.static_pages import pages
 from src.metrics.router import metrics
 from src.metrics.middleware import logging_for_metrics
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -30,7 +30,9 @@ app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
 # exception_response(app) 
 
-app.include_router(pass_router, tags=["PASSWORDS"])
-app.include_router(users_router, tags=["USERS"])
+app.include_router(api_passwords, tags=["PASSWORDS", "API"], prefix="/api")
+app.include_router(api_users, tags=["USERS", "API"], prefix="/api")
+app.include_router(web_passwords, tags=["PASSWORDS", "WEB"], prefix="/web")
+app.include_router(web_users, tags=["USERS", "WEB"], prefix="/web")
 app.include_router(pages, tags=["PAGES"])
 app.include_router(metrics, tags=["METRICS"])
--- src/routers/api/users.py
+++ src/routers/api/users.py
@@ -7,16 +7,15 @@ from src.schemas.base import RegisterRequestData, LoginRequest, GetUserFields
 from src.dao.userDao import UserDao
 from src.services.secrets import hash_password, verify_password
 from src.core.logger import get_logger
-from src.services.cookies import add_bearer_cookie, add_refresh_cookie
 
 logger = get_logger(__name__)
 
-users = APIRouter()
+api_users = APIRouter()
 
 #==============================
 #            API   
 #==============================
-@users.post("/api/register", status_code=201)
+@api_users.post("/register", status_code=201)
 async def register_post(
     user_data: RegisterRequestData,
     db=Depends(get_db),
@@ -56,7 +55,7 @@ async def register_post(
         raise HTTPException(500, "Internal server error")
 
 
-@users.post("/api/login")
+@api_users.post("/login")
 async def login_post(
     user_data: LoginRequest,
     db=Depends(get_db),
@@ -91,7 +90,7 @@ async def login_post(
         raise HTTPException(500, "Internal server error")
     
     
-@users.get("/api/refresh")
+@api_users.get("/refresh")
 async def refresh_get(
     user_id = Depends(verify_refresh_token)
 ):
@@ -113,7 +112,7 @@ async def refresh_get(
         raise HTTPException(500, "Internal server error")
     
 
-@users.patch("/api/change-password")
+@api_users.patch("/change-password")
 async def change_password(
     current_password: str = Body(...),
     new_password: str = Body(...), 
@@ -145,111 +144,3 @@ async def change_password(
     
 
 
-#==============================
-#            WEB    
-#==============================
-from src.dependincies import verify_web_refresh_token
-
-@users.post("/web/register", status_code=201)
-async def web_register_post(
-    user_data: RegisterRequestData,
-    db=Depends(get_db),
-):
-    try:
-        password_hash = hash_password(user_data.password)
-        user_data.password = password_hash
-
-        dao = UserDao(db)
-
-        exist_username = await dao.get_user_by_field(
-            GetUserFields.USERNAME, user_data.username
-        )
-        if exist_username:
-            raise HTTPException(409, detail="User with this username alredy exists")
-
-        if user_data.telegram_id is not None:
-            exist_telegram_id = await dao.get_user_by_field(
-            GetUserFields.TELEGRAM_ID, user_data.telegram_id
-        )
-            if exist_telegram_id:
-                raise HTTPException(409, detail="User with this telegram id alredy exists")
-
-        await dao.create_user(user_data)
-
-        response = MessageResponse(
-            ok=True,
-            detail="New user was sucessfully created"
-        )
-        return response
-
-    except HTTPException as e:
-        logger.warning(e)
-        raise e
-    except Exception as e:
-        logger.exception(e)
-        raise HTTPException(500, "Internal server error")
-
-
-@users.post("/web/login")
-async def web_login_post(
-    user_data: LoginRequest,
-    db=Depends(get_db),
-):
-    try:
-        dao = UserDao(db)
-        user = await dao.get_user_by_field(GetUserFields.USERNAME, user_data.username)
-        if not user:
-            raise HTTPException(401, "Wrong user data")
-
-        if not verify_password(user_data.password, user.password_hash):
-            raise HTTPException(401, "Wrong user data")
-
-        jwt_service = get_jwt_service()
-        bearer_token = jwt_service.create_access_token(user_id=user.id)
-        refresh_token = jwt_service.create_refresh_token(user_id=user.id)
-        
-        content = {
-            "ok": True,
-            "detail": "Success login"
-        } 
-        
-        response = JSONResponse(
-            content=content
-        )
-        add_bearer_cookie(response=response, value=bearer_token.token)
-        add_refresh_cookie(response=response, value=refresh_token.token)
-        
-        return response
-    
-    except HTTPException as e:
-        logger.warning(e)
-        raise e
-    except Exception as e:
-        logger.exception(e)
-        raise HTTPException(500, "Internal server error")
-    
-    
-@users.get("/web/refresh")
-async def web_refresh_get(
-    user_id = Depends(verify_web_refresh_token)
-):
-    try:
-        jwt_service = get_jwt_service()
-        bearer_token = jwt_service.create_access_token(user_id=user_id)
-        refresh_token = jwt_service.create_refresh_token(user_id=user_id)
-        
-        content = content = {
-            "ok": True,
-            "detail": "Tokens were succussefully refreshed"
-        } 
-        response = JSONResponse(
-            content=content
-        )
-        add_bearer_cookie(response=response, value=bearer_token.token)
-        add_refresh_cookie(response=response, value=refresh_token.token)
-        
-        return response
-    
-    except Exception as e:
-        logger.exception(e)
-        raise HTTPException(500, "Internal server error")
